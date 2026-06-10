7eb7a65fde21d565b3caf8ef21b59f5c12516dfb
wivallee <wivallee@f6r12s13.paris.42.school>
2026-06-10 14:52:26 +0200

    added who but does not fix irssi /msg issue

--- Makefile
+++ Makefile
@@ -21,7 +21,7 @@ INCLUDES		= $(wildcard $(INCLUDES_DIR)/*.hpp) \
 
 # Sources
 SRCS_DIR		= srcs/
-SRCS_FILES			= main.cpp Client.cpp Serveur.cpp Channel.cpp parsecommande.cpp handler.cpp commandhandler.cpp JoinCommand.cpp KickCommand.cpp InviteCommand.cpp TopicCommand.cpp ModeCommand.cpp PrivMsg.cpp WhoIs.cpp
+SRCS_FILES			= main.cpp Client.cpp Serveur.cpp Channel.cpp parsecommande.cpp handler.cpp commandhandler.cpp JoinCommand.cpp KickCommand.cpp InviteCommand.cpp TopicCommand.cpp ModeCommand.cpp PrivMsg.cpp Who.cpp WhoIs.cpp
 
 SRCS			= $(addprefix $(SRCS_DIR), $(SRCS_FILES))
 SRCS_BONUS		= $(addprefix $(SRCS_DIR), $(SRCS_MAIN_BONUS)) $(addprefix $(SRCS_DIR), $(SRCS_FILES))
---
+++ includes/Who.hpp
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Who.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: buranchiman <buranchiman@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/26 16:04:19 by buranchiman       #+#    #+#             */
+/*   Updated: 2026/05/26 16:11:54 by buranchiman      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include <vector>
+#include "Command.hpp"
+#include "Channel.hpp"
+
+class Who: public Command
+{
+private:
+	std::vector<Client *> *clients_;
+	std::vector<Channel> *channels_;
+
+public:
+	Who(std::vector<Client *> &clients, std::vector<Channel> &channels);
+	~Who();
+
+	/**
+	 * @brief Execute Who command
+	 *
+	 * @param client Client sending the private message
+	 * @param args Arguments after Who command
+	 * @return void
+	 */
+	void execute(Client &client, const std::string &args);
+
+private:
+	/**
+	 * @brief Internal logic for Who
+	 *
+	 * @param client Client requesting to send the message
+	 * @param msg Raw arguments after Who
+	 * @return void
+	 */
+
+};
